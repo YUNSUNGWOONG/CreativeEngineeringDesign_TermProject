1c4dfee222b564c61274ed1556e382a50c0f2382
JackSmith-f <2170004487f@gmail.com>
2024-01-16 21:37:41 +0900

    d

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CreativeEngineeringDesign_TermProject
 창의공학설계 텀프로젝트- PCSAPS(보행자보호시스템) 설계제작 및 구현
+
+
+
+
+
+[![PCSAPS시연영상][2]][1]
+
+[1]: https://www.youtube.com/watch?v=2XYPsuYIMac
+[2]: preview(20240116).png(live preview)
+
+
---
+++ ref)Arduino/remoteControlCar.ino
@@ -0,0 +1,171 @@
+#include <AFMotor.h>
+#include <SoftwareSerial.h>
+
+int Tx = 9;
+int Rx = 10;
+SoftwareSerial mySerial(Tx, Rx); // RX, TX
+
+//1번 모터만 제어
+AF_DCMotor motor1(1);
+
+//2번 모터만 제어
+AF_DCMotor motor2(2);
+
+//3번 모터만 제어
+AF_DCMotor motor3(3);
+
+//4번 모터만 제어
+AF_DCMotor motor4(4);
+
+String ms = "";
+int num = 5;
+int sp=180; //모터속도를 180으로 설정
+int a;
+
+void setup() {
+  //블루투스 모듈을 9600이라는 속도로 사용
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
+  //1번 모터 속도설정
+  motor1.setSpeed(sp);
+  //1번 모터가 정지해있게 초기설정
+  motor1.run(RELEASE);
+
+  //2번 모터 속도설정
+  motor2.setSpeed(sp);
+  //2번 모터가 정지해있게 초기설정
+  motor2.run(RELEASE);
+
+  //3번 모터 속도설정
+  motor3.setSpeed(sp);
+  //3번 모터가 정지해있게 초기설정
+  motor3.run(RELEASE);
+
+  //4번 모터 속도설정
+  motor4.setSpeed(sp);
+  //4번 모터가 정지해있게 초기설정
+  motor4.run(RELEASE);
+
+}
+
+void loop(){
+  if (mySerial.available()) {       
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {         
+    mySerial.write(Serial.read());
+  }
+  
+   
+
+  ms = "";
+
+  while(mySerial.available()){
+    ms = mySerial.readStringUntil('c');
+    a = ms.toInt();
+    delay(10);
+  }
+  
+
+  //받아온 a값이 9보다 크면, 속도로 들어가고, 그렇지 않으면 num으로 들어가!
+  if(a>9){
+    Serial.print("a>9: ");
+    Serial.println(a);
+    sp = a;
+  }else{
+    num = a;
+  }
+  
+  if (num == 1){
+    delay(10);
+    motor1.setSpeed(sp);
+    motor2.setSpeed(sp);
+    motor3.setSpeed(sp);
+    motor4.setSpeed(sp);
+    motor1.run(RELEASE);
+    motor2.run(RELEASE);
+    motor3.run(FORWARD);
+    motor4.run(FORWARD);
+    delay(10);
+  }else if(num == 2){
+    motor1.setSpeed(sp);
+    motor2.setSpeed(sp);
+    motor3.setSpeed(sp);
+    motor4.setSpeed(sp);
+    motor1.run(FORWARD);
+    motor3.run(BACKWARD);
+    motor2.run(BACKWARD);
+    motor4.run(FORWARD);
+    delay(10);
+  }else if(num == 3){
+    Serial.println(num);
+    motor1.setSpeed(sp);
+    motor2.setSpeed(sp);
+    motor3.setSpeed(0);
+    motor4.setSpeed(0);
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    motor3.run(RELEASE);
+    motor4.run(RELEASE);
+    delay(10);
+  }else if(num == 4){
+    motor1.setSpeed(sp);
+    motor2.setSpeed(sp);
+    motor3.setSpeed(sp);
+    motor4.setSpeed(sp);
+    motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+    motor3.run(FORWARD);
+    motor4.run(FORWARD);
+    delay(10);
+  }else if(num == 5){
+    motor1.run(RELEASE);
+    motor2.run(RELEASE);
+    motor3.run(RELEASE);
+    motor4.run(RELEASE);
+    delay(10);
+  }else if(num == 6){
+    motor1.setSpeed(sp);
+    motor2.setSpeed(sp);
+    motor3.setSpeed(sp);
+    motor4.setSpeed(sp);
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    motor3.run(BACKWARD);
+    motor4.run(BACKWARD);
+    delay(10);
+  }else if(num == 7){
+      motor1.setSpeed(sp);
+      motor2.setSpeed(sp);
+      motor3.setSpeed(sp);
+      motor4.setSpeed(sp);
+      motor1.run(RELEASE);
+      motor2.run(RELEASE);
+      motor3.run(BACKWARD);
+      motor4.run(BACKWARD);
+      delay(10);
+  }else if(num == 8){
+    motor1.setSpeed(sp);
+    motor2.setSpeed(sp);
+    motor3.setSpeed(sp);
+    motor4.setSpeed(sp);
+    motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+    motor3.run(BACKWARD);
+    motor4.run(BACKWARD);
+    delay(10);
+  }else if(num == 9){
+    motor1.setSpeed(sp);
+    motor2.setSpeed(sp);
+    motor3.setSpeed(sp);
+    motor4.setSpeed(sp);
+    motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+    motor3.run(RELEASE);
+    motor4.run(RELEASE);
+    delay(10);
+  }
+
+
+}
